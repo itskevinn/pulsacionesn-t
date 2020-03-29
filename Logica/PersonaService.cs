1d9fe06a8d9e5543fb976cb33b89449d0ccf5f2b
itskevinn <kevinponton@unicesar.edu.co>
2020-03-28 20:18:56 -0500

    tercer commit

--- Logica/PersonaService.cs
+++ Logica/PersonaService.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 
+using Datos;
+
+using Entity;
+
 namespace Logica {
     public class PersonaService {
         private readonly ConnectionManager _conexion;
@@ -21,13 +25,25 @@ namespace Logica {
                 return new GuardarPersonaResponse ($"Error de la Aplicacion: {e.Message}");
             } finally { _conexion.Close ( ); }
         }
-        
-        public List<Persona> ConsultarTodos ( )
-        {
+
+        public List<Persona> ConsultarTodos ( ) {
             _conexion.Open ( );
             List<Persona> personas = _repositorio.ConsultarTodos ( );
             _conexion.Close ( );
             return personas;
+        }
+        public class GuardarPersonaResponse {
+            public GuardarPersonaResponse (Persona persona) {
+                Error = false;
+                Persona = persona;
+            }
+            public GuardarPersonaResponse (string mensaje) {
+                Error = true;
+                Mensaje = mensaje;
+            }
+            public bool Error { get; set; }
+            public string Mensaje { get; set; }
+            public Persona Persona { get; set; }
         }
     }
 }
